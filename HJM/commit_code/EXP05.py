a060f412c3ca78fb9d4bd2202ee5bc9bd4d1f012
HongJaeMin <hjm7392@mail.ulsan.ac.kr>
2024-06-24 10:48:59 +0000

    feat: Use RecursiveCharacterTextSplitter

--- HJM/commit_code/EXP05.py
+++ HJM/commit_code/EXP05.py
@@ -7,7 +7,7 @@ import time
 import pandas as pd
 
 from langchain import hub
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import JSONLoader
 from langchain.schema import Document
 from langchain_community.vectorstores import FAISS
@@ -64,13 +64,15 @@ for tmp in temp:
 # # 문장 최소 길이 = 44, 평균 길이 = 315, 최대 길이 = 1230
 
 
-# # Split 생략
-# splitter = RecursiveCharacterTextSplitter(
-#     chunk_size=300,
-#     chunk_overlap=20,
-# )
-# # pdf나 기사 텍스트처럼 긴 텍스트가 아니라고 생각돼서 생략함
-# # Splitter를 수행해도 문서 당 2개 정도로 분리됨
+# Split
+splitter = RecursiveCharacterTextSplitter(
+    separators=['\n\n', '. ', ' ', ''],
+    chunk_size=100,
+    chunk_overlap=0,
+)
+split_documents = splitter.split_documents(documents)
+
+print(f'Text Splitter 적용 전 문서 개수: {len(documents)}\nText Splitter 적용 후 문서 개수: {len(split_documents)}')
 
 
 # Embedding
@@ -86,7 +88,7 @@ folder_path = f'./faiss_{LANGCHAIN_PROJECT}'
 if not os.path.exists(folder_path):
     print('Vector Store 생성 중')
     vectorstore = FAISS.from_documents(
-        documents=documents,
+        documents=split_documents,
         embedding=embeddings,
     )
     vectorstore.save_local(folder_path=folder_path)
@@ -126,66 +128,61 @@ else:
 #     i += 1
 
 
-###########################
-# 과학 지식 질의가 아닌 경우, 검색하지 않고 적절한 답변을 생성하도록 해야 함
-###########################
+# RAG 구현에 필요한 Question Answering을 위한 LLM  프롬프트
+prompt = hub.pull("rlm/rag-prompt")
 
 
-# # RAG 구현에 필요한 Question Answering을 위한 LLM  프롬프트
-# prompt = hub.pull("rlm/rag-prompt")
+# LLM과 검색엔진을 활용한 RAG 구현
+retriever = vectorstore.as_retriever(k=4)
+chat = ChatUpstage(model='solar-1-mini-chat', temperature=0)
 
 
-# # LLM과 검색엔진을 활용한 RAG 구현
-# retriever = vectorstore.as_retriever(k=4)
-# chat = ChatUpstage(model='solar-1-mini-chat', temperature=0)
+def format_docs(docs):
+    global references
+    references = docs
+    return '\n\n'.join(doc.page_content for doc in docs)
+
+def answer_question(messages):
+    global references
+    response = {"topk": "", "answer": "", "references": ""}
+
+    rag_chain = (
+        {'context': retriever | format_docs, 'question': RunnablePassthrough()}
+        | prompt
+        | chat
+        | StrOutputParser()
+    )
+
+    history = '\n'.join([f"{message['role']}: {message['content']}" for message in messages]) + '\n'
+    answer = rag_chain.invoke(history)
+
+    ref_content = [reference.page_content for reference in references]
+    topk = [reference.metadata['docid'] for reference in references]
+
+    response["topk"] = topk
+    response["answer"] = answer
+    response["references"] = ref_content
+
+    return response
 
 
-# def format_docs(docs):
-#     global references
-#     references = docs
-#     return '\n\n'.join(doc.page_content for doc in docs)
+# 평가를 위한 파일을 읽어서 각 평가 데이터에 대해서 결과 추출후 파일에 저장
+def eval_rag(eval_filename, output_filename):
+    with open(eval_filename) as eval_lines, open(output_filename, 'w') as output_lines:
+        idx = 0
+        for eval_line in eval_lines:
+            j = json.loads(eval_line)
+            print(f'Test {idx}\nQuestion: {j["msg"]}')
+            response = answer_question(j["msg"])
+            print(f'Answer: {response["answer"]}\n')
 
-# def answer_question(messages):
-#     global references
-#     response = {"topk": "", "answer": "", "references": ""}
+            # 대회 score 계산은 topk 정보를 사용, answer 정보는 LLM을 통한 자동평가시 활용
+            output = {"eval_id": j["eval_id"], "topk": response["topk"], "answer": response["answer"], "references": response["references"]}
+            output_lines.write(f'{json.dumps(output, ensure_ascii=False)}\n')
+            idx += 1
 
-#     rag_chain = (
-#         {'context': retriever | format_docs, 'question': RunnablePassthrough()}
-#         | prompt
-#         | chat
-#         | StrOutputParser()
-#     )
+# 평가 데이터에 대해서 결과 생성 - 파일 포맷은 jsonl이지만 파일명은 csv 사용
+eval_rag('../data/eval.jsonl', '../submit/EXP05.csv')
 
-#     history = '\n'.join([f"{message['role']}: {message['content']}" for message in messages]) + '\n'
-#     answer = rag_chain.invoke(history)
-
-#     ref_content = [reference.page_content for reference in references]
-#     topk = [reference.metadata['docid'] for reference in references]
-
-#     response["topk"] = topk
-#     response["answer"] = answer
-#     response["references"] = ref_content
-
-#     return response
-
-
-# # 평가를 위한 파일을 읽어서 각 평가 데이터에 대해서 결과 추출후 파일에 저장
-# def eval_rag(eval_filename, output_filename):
-#     with open(eval_filename) as eval_lines, open(output_filename, 'w') as output_lines:
-#         idx = 0
-#         for eval_line in eval_lines:
-#             j = json.loads(eval_line)
-#             print(f'Test {idx}\nQuestion: {j["msg"]}')
-#             response = answer_question(j["msg"])
-#             print(f'Answer: {response["answer"]}\n')
-
-#             # 대회 score 계산은 topk 정보를 사용, answer 정보는 LLM을 통한 자동평가시 활용
-#             output = {"eval_id": j["eval_id"], "topk": response["topk"], "answer": response["answer"], "references": response["references"]}
-#             output_lines.write(f'{json.dumps(output, ensure_ascii=False)}\n')
-#             idx += 1
-
-# # 평가 데이터에 대해서 결과 생성 - 파일 포맷은 jsonl이지만 파일명은 csv 사용
-# eval_rag('../data/eval.jsonl', '../submit/EXP03.csv')
-
-# # LangSmith 저장 시간 확보
-# time.sleep(60)
+# LangSmith 저장 시간 확보
+time.sleep(60)
